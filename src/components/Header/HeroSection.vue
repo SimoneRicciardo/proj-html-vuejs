a6037e1c9f3821cddd87ee183c5714a672896328
simone ricciardo <you@example.com>
2022-09-05 16:57:17 +0200

    stilizzazione hero scss da  fare

--- src/components/Header/HeroSection.vue
+++ src/components/Header/HeroSection.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    
+  <div class="container-hero">
+    <img src="@/images/artist-shape-03.png" alt="">
+    <img src="@/images/artist-hero-image-01.jpg" alt="">
+    <img src="@/images/artist-hero-image-02-232x300.jpg" alt="">
+    <img src="@/images/artist-hero-image-04-235x300.jpg" alt="">
+    <img src="@/images/artist-shape-05.png" alt="">
+    <img src="@/images/artist-shape-04-127x300.png" alt="">
+    <img src="@/images/artist-shape-03-201x300.png" alt="">
+    <div class="info-hero">
+      <span>
+        Hello,Im Matin
+      </span> 
+      <span>
+        Artist Coaching And Mentoring Might Be for You
+      </span>
+      <button>Get started today</button>
+    </div>
   </div>
 </template>
 
@@ -11,5 +26,16 @@ export default {
 </script>
 
 <style lang="scss">
+.container-hero{
+  height: 800px;
+  background: url(@/images/artist-shape-01.png), url(@/images/artist-shape-02-768x415.png),  rgb(251, 249, 246);
+  background-repeat: no-repeat;
+  background-position-x: right, 0;
 
+    .info-hero{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+}
 </style>
